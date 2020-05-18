470e2441c6bd058aa3f3ac3e87fb03937a4af61d
Sean Kuusinen <seankuusinen@gmail.com>
2020-05-18 17:19:47 +0000

    excercise 2

---
+++ the_basics/2.js
@@ -0,0 +1,21 @@
+let number = 4936
+let ones = number % 10
+ones
+
+
+number = (number - ones) / 10
+493
+
+let tens = number % 10
+tens
+
+
+number = (number - tens) / 10
+
+let hundreds = number % 10
+hundreds
+
+
+let thousands = (number - hundreds) / 10
+thousands
+
